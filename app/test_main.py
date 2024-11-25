790bff73149a832166b4eab830990213d4644ff1
Alex Plugin <plugin0785@gmail.com>
2024-11-25 14:34:51 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -10,17 +10,17 @@ def test_should_return_pennies() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def test_should_return_one_nickels() -> None:
+def test_should_return_nickels() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_should_return_one_dimes() -> None:
+def test_should_return_dimes() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def test_should_return_one_quarters() -> None:
+def test_should_return_quarters() -> None:
     assert get_coin_combination(50) == [0, 0, 0, 2]
 
 
-# def test_should_return_value_error_if_negative() -> None:
-#     assert get_coin_combination(-1) == ValueError
+def test_should_return_empty_test_for_zero() -> None:
+    assert get_coin_combination(00) == [0, 0, 0, 0]
